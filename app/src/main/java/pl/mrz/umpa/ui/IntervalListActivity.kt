cda70519d10e600844f31ca423945932d973d83a
rzepkamichal <mrzepka@protonmail.com>
2020-06-18 22:16:00 +0200

    Design interval list view

---
+++ app/src/main/java/pl/mrz/umpa/ui/IntervalListActivity.kt
@@ -0,0 +1,12 @@
+package pl.mrz.umpa.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.mrz.umpa.R
+
+class IntervalListActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intervallist)
+    }
+}
