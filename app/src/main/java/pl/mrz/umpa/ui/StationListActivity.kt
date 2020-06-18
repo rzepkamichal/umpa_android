70c7f8402bcb15d1ed9ff2626f5db37881c99c89
rzepkamichal <mrzepka@protonmail.com>
2020-06-18 15:42:46 +0200

    Design stationlist view

---
+++ app/src/main/java/pl/mrz/umpa/ui/StationListActivity.kt
@@ -0,0 +1,12 @@
+package pl.mrz.umpa.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.mrz.umpa.R
+
+class StationListActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stationlist)
+    }
+}
